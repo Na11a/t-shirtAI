86c8700b3e5a843a773f5dcbf92df89203707376
Na11a <simmax.in.ua@gmail.com>
2023-09-16 19:45:42 +0300

    create t-shirt template

--- client/src/canvas/index.jsx
+++ client/src/canvas/index.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import { Canvas } from '@react-three/fiber'
+import { Environment, Center} from 
+'@react-three/drei'
+import Shirt from './Shirt'
+import Backdrop from './Backdrop'
+import CameraRig from './CameraRig'
 
-function Canvas() {
+function CanvasModel() {
   return (
-    <div>Canvas</div>
+    <Canvas>
+      <ambientLight intensity={0.5}/>
+      <Environment preset='city'/>
+        <Center>
+          <Shirt />
+        </Center>
+    </Canvas>
   )
 }
 
-export default Canvas
+export default CanvasModel
