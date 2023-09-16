99731f1279cb634ed36e01e3bd44e93a29edb1a4
Na11a <simmax.in.ua@gmail.com>
2023-09-16 18:49:36 +0300

    home page init structure and styles

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,6 @@ import Customizer from "./pages/Customizer"
 import Home from "./pages/Home"
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <main className="app transition-all ease-in">
